e29d6d33ad1ca089f228eea8431970c2c0552682
nakademasaki-ui <nakade.masaki@gmail.com>
2026-09-01 20:35:27 +0900

    Add files via upload

--- js/data/syncMeta.js
+++ js/data/syncMeta.js
@@ -1,7 +1,7 @@
 // Auto-generated metadata by scripts/update_data.py
 export const SYNC_META = {
-  lastUpdatedUtc: '2026-08-30 08:05 UTC',
-  lastUpdatedJst: '2026年08月30日 17:05 JST',
+  lastUpdatedUtc: '2026-09-01 11:30 UTC',
+  lastUpdatedJst: '2026年09月01日 20:30 JST',
   autoSyncActive: true,
   itemsSyncedCount: 12,
   newsCount: 12,
